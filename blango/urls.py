6e3b924ebf71f9c0c4ff645bc4022074ab3a2a6d
Nikita Pelenskii <nikita.pelenskii@icloud.com>
2022-05-11 18:09:16 +0000

    Finish DRF views

--- blango/urls.py
+++ blango/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path("ip/", blog.views.get_ip),
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
-    path("api/v1/", include("blog.api_urls"))
+    path("api/v1/", include("blog.api.urls"))
 
 ]
 
