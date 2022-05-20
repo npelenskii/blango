0013e583169a6cfeb4b51bb9e1d7603b940ccaa0
Nikita Pelenskii <nikita.pelenskii@icloud.com>
2022-05-20 11:40:10 +0000

    Finish fetch and React hooks

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 import logging
 
+from django.urls import reverse
 from blog.forms import CommentForm
 from blog.models import Post
 
@@ -16,7 +17,9 @@ def get_ip(request):
 
 
 def post_table(request):
-    return render(request, "blog/post-table.html")
+    return render(
+        request, "blog/post-table.html", {"post_list_url": reverse("post-list")}
+    )
 
 
 def index(request):
